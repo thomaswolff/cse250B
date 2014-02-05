616b1a4064efc496beecb424c013ef317ddf3b20
Thomas Wolff <thomaswolff90@gmail.com>
2014-02-05 15:45:50 -0800

    Added latest changes to crf.py

--- a2/crf.py
+++ a2/crf.py
@@ -9,10 +9,12 @@ EXCAMLATION_POINT = 4
 COLON = 5
 STOP = 7
 
-minint = -100000000000
+minint = -100000
 
-question_words = {"how", "where", "who", "why", "when"}
-conjunctions_words = {"and", "or", "but", "for", "as", "because", "nor", "yet", "so"}
+question_words = ["how", "where", "who", "why", "when", "what", "can"]
+conjunction_words = ["and", "or", "but", "for", "as", "because", "nor", "yet", "so"]
+indictment_words = ["hi", "hello", "hey"]
+tag_set = [SPACE, PERIOD, COMMA, QUESTION_MARK, EXCAMLATION_POINT, COLON]
 
 class Sentence(object):
 	x = []
@@ -23,43 +25,125 @@ class Sentence(object):
 		self.x = x
 		self.y = y
 		self.predicted_y = []
+
+class FeatureFunction(object):
+	a_fun = None
+	b_fun = None
+	
+	def __init__(self, a_fun, b_fun):
+		self.a_fun = a_fun
+		self.b_fun = b_fun
+	
+	def __call__(self, words, tags):
+		sum = 0.0
+		for i in range(len(words)):
+			y = tags[i]
+			y_prev = START
+			if(i is not 0): y_prev = tags[i-1]
+			a_result = self.a_fun(words, i)
+			if(a_result is not 0):
+				sum += a_result * self.b_fun(y_prev, y)
+		return sum
 		
+	def evaluateLowLevelFeatureFunction(self, y_prev, y, words, i):
+		a_result = self.a_fun(words, i)
+		if(a_result is 0): return 0.0
+		else: return a_result * self.b_fun(y_prev, y)
+	
+	
 def featureFunction1(y_prev, y, x, i):
 	return y is QUESTION_MARK and x[0].lower() in question_words and i == len(x) - 1
 
 
-def fFsQuestionTemplate(k):
+def fFsQuestionTemplate(tag):
 	def f(y_prev, y, x, i):
-		return y is QUESTION_MARK and x[k].lower() in question_words and i == len(x) - 1
+		return y is tag and x[0].lower() in question_words and i == len(x) - 1
 	return f
 
-def fFsEndsWithTemplate(k, suffix):
+def fFsEndsWithTemplate(suffix, tag):
 	def f(y_prev, y, x, i):
-		return x[k].endswith(suffix)
+		return x[i].endswith(suffix)
 	return f
 
-def fFsStartWithCapitalLetter(k):
+def fFsStartWithCapitalLetter(tag):
 	def f(y_prev, y, x, i):
-		return x[k][0].isupper()
+		return x[i][0].isupper() and y is tag
 	return f
 
-def fFsStartssWithTemplate(k, suffix):
+def fFsStartssWithTemplate(suffix, tag):
 	def f(y_prev, y, x, i):
-		return x[k].startswith(suffix)
+		return x[i].startswith(suffix)
 	return f
 
-def fFsConjunction(k):
-	def f(y_prev, y, x, i):
-		return y is COMMA and x[k].lower() in conjunctions_words
+def fFsConjunction(tag):
+	def f(y_prev, y, words, i):
+		return y is tag and words[i].lower() in conjunctions_words
 	return f
 
-def fFsPeriod(k):
-	def f(y_prev, y, x, i):
-		return k is i and y is PERIOD
+def fFsTag(tag):
+	def f(y_prev, y, words, i):
+		return y is tag
+	return f
+	
+def fFsTagInLastPostion(tag):
+	def f(y_prev, y, words, i):
+		return y is tag and i is (len(words)-1)
 	return f
 
+# A functions
+def startsWithCapitalLetter(x, i):
+	return x[i][0].isupper()
 
+def wordStartsWithSequence(a):
+	def f(x, i):
+		return x[i].startswith(a)
+	return f
 
+def wordEndsWithSequence(a):
+	def f(x, i):
+		return x[i].endswith(a)
+	return f
+
+def wordIs(a):
+	def f(x, i):
+		return x[i].lower() == a
+	return f
+
+# B functions
+def tagIs(b):
+	def f(y_prev, y):
+		return y == b
+	return f
+
+def prevTagIs(b):
+	def f(y_prev, y):
+		return y_prev == b
+	return f
+
+def tagsAre(b):
+	def f(y_prev, y):
+		return y_prev == b[0] and y == b[1]
+	return f
+	
+
+def initializeFeatureFunctions():
+	f = []
+	a_functions = []
+	b_functions = []
+	for a in {"ing", "s"}:
+		a_functions.append(wordEndsWithSequence(a))
+	for a in conjunction_words + question_words:
+		a_functions.append(wordIs(a))
+	for tag in tag_set:
+		b_functions.append(tagIs(tag))
+		b_functions.append(prevTagIs(tag))
+		for tag_2 in tag_set:
+			b_functions.append(tagsAre([tag, tag_2]))
+	for a_func in a_functions:
+		for b_func in b_functions:
+			f.append(FeatureFunction(a_func, b_func))
+	print("Number of feature functions: " + str(len(f)))
+	return f
 
 
 	
@@ -75,7 +159,7 @@ def featureFunction2(y_prev, y, x, i):
 def g(y_prev, y, w, f, x, i):
 	sum = 0.0
 	for (weight, function) in zip(w, f):
-		sum += weight*function(y_prev, y, x, i)
+		sum += weight*function.evaluateLowLevelFeatureFunction(y_prev, y, x, i)
 	return sum
 	
 # Calculate all g functions for all y_prev and y given a sentence x
@@ -100,12 +184,8 @@ def preprocess(x, w, f, tag_set):
 # gs are the different enumerated g-functions
 def U(k, v, gs, tag_set, dp_table):
 	max_val = minint
-	print "k: " + str(k)
 	if(k is 1):
-		print "er i if"
-
 		for u in tag_set:
-			print "u: " + str(u)
 			if(dp_table[k-1][u] is minint):
 				u_val = gs[k-1][u][v]
 			else:
@@ -113,10 +193,7 @@ def U(k, v, gs, tag_set, dp_table):
 			if (u_val > max_val):
 				max_val = u_val
 		dp_table[0][v] = max_val
-
 	else:
-		print "else"
-		print "u: "
 		for u in tag_set:
 			if(dp_table[k-1][u] is minint):
 				u_val = U(k-1, u, gs, tag_set, dp_table) + gs[k-1][u][v]
@@ -134,10 +211,10 @@ def fill_U_matrix(gs, tag_set, n):
 		U(n, tag, gs, tag_set, dp_table)
 	return dp_table
 
-# Predict the best tag sequence y for sentence x
-def predict(gs, tag_set, x):
+# Predict the most likely tag sequence y for sentence x
+def predict(gs, tag_set, sentence):
 	# Initialize n and y
-	n = len(x)
+	n = len(sentence.x)
 	y = [START for i in range(n)]
 	# Fill U matrix
 	U = fill_U_matrix(gs, tag_set, n)
@@ -160,16 +237,51 @@ def predict(gs, tag_set, x):
 				max_u = u
 		y[k] = max_u
 	return y
-	
-def perceptron():
 
-	train()
-	# Train
-	
-	
-#def validate():
-	# Preprocess
-	# Predict
+def updateWeightsCP(ws, sentence, fs, y_hat):
+	# For each weight, do the update rule in Collins perceptron
+	for i in range(len(ws)):
+		desired = fs[i](sentence.x, sentence.y)
+		#print("F of desired: " + str(desired))
+		undesired = fs[i](sentence.x, y_hat)
+		#print("F of undesired: " + str(undesired))
+		ws[i] += (desired - undesired)
+		
+def collinsPerceptron(fs, tag_set, training_set, validation_set):
+	# Initialize all weights to be zero
+	ws = [0.0]*len(fs)
+	previousCorrectnessRate = 0.0
+	# Epoch loop
+	while(True):
+		# For each sentence in the training data, update weights
+		for i in range(len(training_set)):
+			sentence = training_set[random.randint(0, len(training_set)-1)]
+			gs = preprocess(sentence.x, ws, fs, tag_set)
+			y_hat = predict(gs, tag_set, sentence)
+			updateWeightsCP(ws, sentence, fs, y_hat)
+		print("Epoch done")
+		# See how well the model does
+		correctnessRate = validate(fs, ws, tag_set, validation_set)
+		print("Correctness rate: " + str(correctnessRate))
+		# Break if it begins to do worse than the previous epoch
+		if(correctnessRate <= previousCorrectnessRate): break
+		previousCorrectnessrate = correctnessRate
+	return previousCorrectnessRate
+
+def validate(fs, ws, tag_set, validation_set):
+	numberOfTags = 0
+	numberOfCorrectTags = 0
+	for sentence in validation_set:
+		gs = preprocess(sentence.x, ws, fs, tag_set)
+		y_predicted = predict(gs, tag_set, sentence)
+		#print("Sentence: " + str(sentence.x))
+		#print("y_correct: " + str(sentence.y))
+		#print("y_predicted :" + str(y_predicted))
+		for (correctTag, predictedTag) in zip(sentence.y, y_predicted):
+			numberOfTags += 1
+			if predictedTag is correctTag:
+				numberOfCorrectTags += 1
+	return float(numberOfCorrectTags)/numberOfTags
 	
 def readFile(filename):
 	with open ("./punctuationDataset/" + filename + "Labels.txt") as data:
@@ -182,37 +294,27 @@ def readFile(filename):
 	for (sentence, label) in zip(sentences, labels):
 		wordsInSentence = sentence.rstrip().split(' ')
 		tagsInLabel = label.rstrip().split(' ')
-		examples.append(Sentence(wordsInSentence, tagsInLabel))
+		y = []
+		for tag in tagsInLabel:
+			if(tag == 'SPACE'): y.append(0)
+			if(tag == 'PERIOD'): y.append(1)
+			if(tag == 'COMMA'): y.append(2)
+			if(tag == 'QUESTION_MARK'): y.append(3)
+			if(tag == 'EXCLAMATION_POINT'): y.append(4)
+			if(tag == 'COLON'): y.append(5)
+		examples.append(Sentence(wordsInSentence, y))
 	#random.shuffle(examples)
 	return examples
 
 examples = readFile("training")
-e = examples[19]
-x = e.x
-y = e.y
-#print("Sentence: " + str(x))
-#print("Label: " + str(y))
-w = [10.0]
-f = [featureFunction1]
-tag_set = [SPACE, PERIOD, COMMA, QUESTION_MARK, EXCAMLATION_POINT, COLON]
-gs = preprocess(x, w, f, tag_set)
-#for g in gs:
-#	print("-------G-------")
-#	for y_prev in tag_set:
-#		row = ""
-#		for y in tag_set:
-#			row += " " + str(g[y_prev][y])
-#		print(row)
-#	print("---------------")
-predictLabel = predict(gs, tag_set, x)
-print predictLabel
+print("Examples read")
+training_set = examples[:1000]
+validation_set = examples[1000:1500]
+print("Training set and validation set initialized")
+fs = initializeFeatureFunctions()
+print("Feature functions initialized")
+correctnessRate = collinsPerceptron(fs, tag_set, training_set, validation_set)
 
-A = {}
-B = {}
-fj = []
-for a in A:
-	for b in B:
-		fj = createFeatureFunc(a, b)
 	
 	
 	
